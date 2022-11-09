fbff4c5ee583ec20ef578c4d79ebc57b61b70223
rahul kishor patil <rahulkishorpatil@192.168.1.7>
2022-11-09 13:15:48 +0530

    initial commit

---
+++ greetings/go.mod
@@ -0,0 +1,3 @@
+module github.com/rahulp229/gopackages/greetings
+
+go 1.19
---
+++ greetings/greet.go
@@ -0,0 +1,7 @@
+package greetings
+
+import "fmt"
+
+func greetMessage(name string) {
+	fmt.Printf("Hello %v \n", name)
+}
